73b7ebdfaf16e5b58c86288d1f365e5d10f2d091
Igor <igorpollo@gmail.com>
2014-12-28 23:43:53 -0200

    com ajax

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -5,6 +5,7 @@ class FeedsController < ApplicationController
   # GET /feeds.json
   def index
     @feeds = Feed.all.reverse
+    @feed = Feed.new
   end
 
   # GET /feeds/1
@@ -14,7 +15,12 @@ class FeedsController < ApplicationController
 
   # GET /feeds/new
   def new
+    if (params[:texto])
+      @feed = current_user.feeds.new(texto: :texto, place_id: place_id)
+      @feed.save
+    else
     @feed = Feed.new
+    end
   end
 
   # GET /feeds/1/edit
